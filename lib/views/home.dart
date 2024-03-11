27bf515f9294e4c2d181bc4c1a9dbe03d78027e6
harshitdixit14 <113369159+harshitdixit14@users.noreply.github.com>
2024-03-11 20:10:30 +0530

    news app in flutter

---
+++ lib/views/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _State createState()=>_State();
+}
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
